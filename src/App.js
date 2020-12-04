3bb038bdc3bf2c45d5b567f03295b9c6b9f6124c
Bew <Bew@sysops-MBP.broadband>
2020-12-04 14:11:57 +0000

    useState in functional component, but issue with the re-rendering too many times

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import moment from 'moment';
 
-import Calendar from './calendar';
 import './App.css';
 
 function App() {
-  const getMoment = moment();
-  const startDay = getMoment.clone().startOf('month').startOf('isoWeek');
-  const endDay = getMoment.clone().endOf('month').endOf('isoweek');
+  const [calendar, setCalendar] = useState([]);
+  const [setMoment, getMoment] = useState(moment());
+
+  const startDay = setMoment.clone().startOf('month').startOf('isoWeek');
+  const endDay = setMoment.clone().endOf('month').endOf('isoweek');
   const day = startDay.clone().subtract(1, 'day');
-  console.log(day);
-  const calendar = [];
+  // console.log(day);
+
+  const temp = [];
 
   while (day.isBefore(endDay, 'day')) {
     // use moment method
@@ -22,6 +24,8 @@ function App() {
         .map(() => day.add(1, 'day').clone())
     );
   }
+
+  setCalendar(temp);
   console.log(calendar);
   return (
     <div className="App">

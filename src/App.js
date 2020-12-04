8701b503ac7c9ab300b51705634cd843a20c3646
Bew <Bew@sysops-MBP.broadband>
2020-12-04 13:05:32 +0000

    get the range of days of that month and render out all days in that month

--- src/App.js
+++ src/App.js
@@ -6,11 +6,32 @@ import './App.css';
 
 function App() {
   const getMoment = moment();
-  const startDay = getMoment.clone().startOf('isoWeek');
-  const endDay = getMoment.clone().endOf('isoweek');
+  const startDay = getMoment.clone().startOf('month').startOf('isoWeek');
+  const endDay = getMoment.clone().endOf('month').endOf('isoweek');
+  const day = startDay.clone().subtract(1, 'day');
+  console.log(day);
+  const calendar = [];
+
+  while (day.isBefore(endDay, 'day')) {
+    // use moment method
+    //if day is still before the end day
+    console.log(endDay);
+    calendar.push(
+      Array(7)
+        .fill(0)
+        .map(() => day.add(1, 'day').clone())
+    );
+  }
+  console.log(calendar);
   return (
     <div className="App">
-      {startDay.format('MM/DD')} - {endDay.format('MM/DD')}{' '}
+      {calendar.map((week) => (
+        <div>
+          {week.map((day) => (
+            <div>{day.format('D').toString()}</div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
